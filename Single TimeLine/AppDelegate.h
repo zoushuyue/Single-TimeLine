ad7e2bdea87ed5963be028df61af3d93e70098af
zoushuyue <1103617692@qq.com>
2016-08-14 10:47:29 +0800

    Initial Commit

---
+++ Single TimeLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Single TimeLine
+//
+//  Created by mac1 on 16/8/14.
+//  Copyright © 2016年 fuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Single TimeLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Single TimeLine
+//
+//  Created by mac1 on 16/8/14.
+//  Copyright © 2016年 fuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
